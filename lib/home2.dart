bd6786740c78f042b4e42028d5d34f3dbd261196
thekeyhi <2thekey@naver.com>
2022-11-28 17:51:13 +0900

    20221128_제외수 작업중

--- lib/home2.dart
+++ lib/home2.dart
@@ -13,7 +13,64 @@ class _Home2State extends State<Home2> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(num[1][1].toString()),
+      child:
+        Column(
+          children: [
+            SizedBox(height: 15,),
+
+
+            InkWell(  //그동안 출현한 번호별 통계
+              onTap: (){
+                int resultcnt=0;
+
+                //for(int i=1; i<last_soonbun-2; i++){
+                  for(int i=1; i<10; i++){
+                  for(int resultImsi=1; resultImsi < (45-num[i][1]);resultImsi++) {
+                    for(int j=1; j<10; j++) {
+                      if (num[i][1] + resultImsi == num[i + 1][1] ||
+                          num[i][1] + resultImsi == num[i + 1][2] ||
+                          num[i][1] + resultImsi == num[i + 1][3] ||
+                          num[i][1] + resultImsi == num[i + 1][4] ||
+                          num[i][1] + resultImsi == num[i + 1][5] ||
+                          num[i][1] + resultImsi == num[i + 1][6]) {
+                        resultcnt++;
+                      } //if
+                    }//for j
+                    //print('결과 = '+resultImsi.toString());
+                  }//for resultImsi
+
+                } //for i
+
+                //print('회차 = '+i.toString());
+
+
+                // setState(() {
+                //
+                //   resultSangtae=3;
+                //
+                // });
+
+              },
+              child:
+              Container(
+                width: disWidthSize,
+                height: 40,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 2
+                    )
+                ),
+                alignment: Alignment.center,
+                child: Text('짝수 & 홀수 출현 횟수', style: TextStyle(fontFamily: 'sandolout', fontSize: 20, fontWeight: FontWeight.bold,  color: Colors.deepPurple),),
+              ),
+            ),
+
+            SizedBox(height: 15,),
+          ],
+        ),
     );
   }
 }
