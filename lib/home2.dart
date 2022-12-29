cd39ced43928d6f4dfc71c9aaa207fc622d77da9
thekeyhi <2thekey@naver.com>
2022-12-29 17:47:26 +0900

    20221229_웹으로전환

--- lib/home2.dart
+++ lib/home2.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home1.dart';
+//import 'lottoad.dart';
 import 'lottovar.dart';
 
 
@@ -12,74 +12,80 @@ class Home2 extends StatefulWidget {
 
 class _Home2State extends State<Home2> {
   @override
+  void initState(){
+    //showInterstitialAd();  //에드몹 전면광고테스트
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       child:
-        Column(
-          children: [
-            SizedBox(height: 15,),
+      Column(
+        children: [
+          SizedBox(height: 15,),
 
 
-            InkWell(  //
-              onTap: (){
-                int resultcnt=0;
-                int targetNum=1;
+          InkWell(  //
+            onTap: (){
+              int resultcnt=0;
+              int targetNum=1;
 
-                //for(int i=1; i<last_soonbun-2; i++){
+              //for(int i=1; i<last_soonbun-2; i++){
 
-                  for(int resultImsi=1; resultImsi < 45;resultImsi++) {
-                    resultcnt=0;
-                    //print('더하기'+resultImsi.toString());
-                    for(int j=1; j< last_soonbun-2 ; j++) {
-                      if (num[j][targetNum] + resultImsi == num[j + 1][1] ||
-                          num[j][targetNum] + resultImsi == num[j + 1][2] ||
-                          num[j][targetNum] + resultImsi == num[j + 1][3] ||
-                          num[j][targetNum] + resultImsi == num[j + 1][4] ||
-                          num[j][targetNum] + resultImsi == num[j + 1][5] ||
-                          num[j][targetNum] + resultImsi == num[j + 1][6]) {
-                        //print('있어 = '+i.toString()+'회'+num[i][1].toString()+'더하기'+resultImsi.toString());
-                        resultcnt++;
-                        //break;
-                      } //if
-                    }//for j
+              for(int resultImsi=1; resultImsi < 45;resultImsi++) {
+                resultcnt=0;
+                //print('더하기'+resultImsi.toString());
+                for(int j=1; j< last_soonbun-2 ; j++) {
+                  if (num[j][targetNum] + resultImsi == num[j + 1][1] ||
+                      num[j][targetNum] + resultImsi == num[j + 1][2] ||
+                      num[j][targetNum] + resultImsi == num[j + 1][3] ||
+                      num[j][targetNum] + resultImsi == num[j + 1][4] ||
+                      num[j][targetNum] + resultImsi == num[j + 1][5] ||
+                      num[j][targetNum] + resultImsi == num[j + 1][6]) {
+                    //print('있어 = '+i.toString()+'회'+num[i][1].toString()+'더하기'+resultImsi.toString());
+                    resultcnt++;
+                    //break;
+                  } //if
+                }//for j
 
-                      print(resultImsi.toString()+'='+resultcnt.toString());
+                print(resultImsi.toString()+'='+resultcnt.toString());
 
-                    //print('결과 = '+resultImsi.toString());
-                  }//for resultImsi
-
-
-
-                //print('회차 = '+i.toString());
+                //print('결과 = '+resultImsi.toString());
+              }//for resultImsi
 
 
-                // setState(() {
-                //
-                //   resultSangtae=3;
-                //
-                // });
 
-              },
-              child:
-              Container(
-                width: disWidthSize,
-                height: 40,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25.0),
-                    border: Border.all(
-                        color: Colors.black,
-                        style: BorderStyle.solid,
-                        width: 2
-                    )
-                ),
-                alignment: Alignment.center,
-                child: Text('두번째 번호에 더하기', style: TextStyle(fontFamily: 'sandolout', fontSize: 20, fontWeight: FontWeight.bold,  color: Colors.deepPurple),),
+              //print('회차 = '+i.toString());
+
+
+              // setState(() {
+              //
+              //   resultSangtae=3;
+              //
+              // });
+
+            },
+            child:
+            Container(
+              width: disWidthSize,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25.0),
+                  border: Border.all(
+                      color: Colors.black,
+                      style: BorderStyle.solid,
+                      width: 2
+                  )
               ),
+              alignment: Alignment.center,
+              child: Text('두번째 번호에 더하기', style: TextStyle(fontFamily: 'sandolout', fontSize: 20, fontWeight: FontWeight.bold,  color: Colors.deepPurple),),
             ),
+          ),
 
-            SizedBox(height: 15,),
-          ],
-        ),
+          SizedBox(height: 15,),
+        ],
+      ),
     );
   }
 }
--- lib/lottovar.dart
+++ lib/lottovar.dart
@@ -1,3 +1,9 @@
+//import 'package:fluttertoast/fluttertoast.dart';
+//import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:flutter/material.dart';
+//import 'lottoad.dart';
+
+
 class MyScore{
   int numLotto;
   int countLotto;
@@ -9,7 +15,12 @@ class MyScore{
   }
 }
 
-int heartCount=10;
+int heartCount=0;
+
+final searchController = TextEditingController();
+var searchTxt='';
+var searchResult='';
+
 
 var ii=0;
 
@@ -44,3 +55,46 @@ String naonTotal=''; //함께 출현한 수 처리시, 선택한 번호묶어서
 // List<int> dangchum3=[0];
 // List<int> dangchum4=[0];
 // List<int> dangchum5=[0]; //배열로 이용
+
+var hiveBox;
+
+void heart_count() async{
+
+  heartCount--;
+  if (heartCount <= 0) {
+    //showRewardedAd();  //리워드전면광고 불러오기
+    //showInterstitialAd();  //에드몹 전면광고로 리워드 광고 대체
+    //lottoToast('하트를 충전하고 있습니다.');
+    heartCount=10;
+  }
+
+  // else {
+  //   hiveBox.put('heart', heartCount);
+  //   //print(heartCount.toString());
+  // }
+}
+
+
+
+// void lottoToast(String jmt_message) {
+//   Fluttertoast.showToast(msg: jmt_message,
+//     gravity: ToastGravity.CENTER,
+//     backgroundColor: Colors.black,
+//     fontSize: 20.0,
+//     textColor: Colors.white,
+//     timeInSecForIosWeb: 1,
+//     toastLength: Toast.LENGTH_SHORT,
+//
+//   );
+// } //토스트메시지 띄우기
+
+void lottoToast(String message,BuildContext context) {
+
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+
+  // Fluttertoast.showToast(
+  //     msg: message,
+  //     backgroundColor: Colors.red,
+  //     toastLength: Toast.LENGTH_SHORT,
+  //     gravity: ToastGravity.BOTTOM_LEFT);
+}
